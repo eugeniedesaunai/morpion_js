2155bccfe6ddaf8ba7f4d268e15540335c77656e
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-08-30 09:05:36 +0200

    last

--- src/components/JoueursPoints.vue
+++ src/components/JoueursPoints.vue
@@ -20,4 +20,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input {
+    color: #f07171;
+    text-shadow: 0px 0px 10px #f07171;
+}
 </style>
